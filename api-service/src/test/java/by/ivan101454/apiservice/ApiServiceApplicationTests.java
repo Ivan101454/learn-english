499c0e053899fb3c44e92adce4c9f85367d29c94
Ivan Sapozhnikov <ivan101454@gmail.com>
2026-02-07 21:20:36 +0300

    add two sub modules view and api, set up parent project

---
+++ api-service/src/test/java/by/ivan101454/apiservice/ApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.ivan101454.apiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
